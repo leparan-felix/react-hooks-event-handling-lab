bc34b9ba0afa18b7e829d72987b055960e82d4e6
leparan <leparanfelix16@gmail.com>
2025-04-11 02:48:33 +0300

    changes

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
-// Code Keypad Component Here
+// Keypad.js
+import React, { useState } from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+const Keypad = () => {
+  const [password, setPassword] = useState('');
+
+  const handleChange = (e) => {
+    setPassword(e.target.value);
+    console.log("Entering password...");
+  };
+
+  return <input type="password" value={password} onChange={handleChange} />;
+};
 
 export default Keypad;
